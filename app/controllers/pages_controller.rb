93282f1c5e388a9ee0ca0618e2b1861aee09725e
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-09-04 19:32:23 +0200

    Remove acme challenge

--- app/controllers/pages_controller.rb
+++
@@ -1,5 +0,0 @@
-class PagesController < ApplicationController
-  def letsencrypt
-    render plain: 'ACWtqH9q36rKbAzuZoe9Uncvo9e1bwPCI5IUfOi-vCs.Omj-5BVzixcKVrHNZZw8QnNC1LmgkPXRs4sGui56ZXc'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
-
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :datasets, only: [ :index ]
