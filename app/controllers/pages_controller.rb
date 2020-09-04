79ef3170f2fa8cb3a9ca2aad5d30a7afc568d00e
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-09-04 19:09:09 +0200

    Add config for acme challenge

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def letsencrypt
+    render plain: 'ACWtqH9q36rKbAzuZoe9Uncvo9e1bwPCI5IUfOi-vCs.Omj-5BVzixcKVrHNZZw8QnNC1LmgkPXRs4sGui56ZXc'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :datasets, only: [ :index ]
