c36dff7b54ad81709f071eadf5b727043e359227
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-09-06 19:15:40 +0200

    Remove search feature as the search will be handled front-end side

--- app/controllers/api/v1/datasets_controller.rb
+++ app/controllers/api/v1/datasets_controller.rb
@@ -1,12 +1,7 @@
 class Api::V1::DatasetsController < ApplicationController
 
   def index
-    search = params['search']
     datasets = JSON.parse(File.read('public/datasets.json'))
-
-    if search
-      datasets = datasets.select { |dataset| dataset['name'].downcase.include? search.downcase }
-    end
 
     render json: datasets
   end
