67778ca3984f88bbbe5e7ade23b7110f04c97c03
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-03-31 02:13:47 +0200

    display all games

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -4,13 +4,7 @@ module Api
   module V1
     class GamesController < ApplicationController
       def index
-        @games =
-          Game
-            .where(
-              start_time:
-                DateTime.now.beginning_of_day..DateTime.now.end_of_day,
-            )
-            .order(id: :desc)
+        @games = Game.order(id: :desc)
         @count = @games.count
       end
 
