fd237893f3c04e136bd41f1e5ca84f6625e88830
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2023-04-15 01:23:38 +0200

    Add "Import videos for Youtuber"

--- app/models/youtuber.rb
+++ app/models/youtuber.rb
@@ -2,4 +2,29 @@ class Youtuber < ApplicationRecord
   validates :channel_id, presence: true, uniqueness: true
 
   has_many :videos, dependent: :destroy
+
+  def import_videos
+    channel.videos.each do |video|
+      exising_video = Video.find_by(video_id: video.id)
+      if exising_video.nil?
+        self.videos.create!(
+          video_id: video.id,
+          title: video.title,
+          description: video.description,
+          thumbnail: video.thumbnail_url(:high),
+          published_at: video.published_at,
+        )
+      else
+        exising_video.update!(
+          title: video.title,
+          description: video.description,
+          thumbnail: video.thumbnail_url(:high),
+        )
+      end
+    end
+  end
+
+  def channel
+    @channel ||= Yt::Channel.new(id: channel_id)
+  end
 end
