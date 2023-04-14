240cf87f81df40366dea7387cbe7e37fcb83138d
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2023-04-14 23:59:09 +0200

    Add validation for Youtuber model and corresponding test

--- app/models/youtuber.rb
+++ app/models/youtuber.rb
@@ -1,2 +1,3 @@
 class Youtuber < ApplicationRecord
+  validates :channel_id, presence: true, uniqueness: true
 end
--- spec/models/youtuber_spec.rb
+++ spec/models/youtuber_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Youtuber, type: :model do
-  it 'has a required columns' do
-    expect(Youtuber.column_names).to eq %w[id subscribers channel_id created_at updated_at]
+  describe 'database columns' do
+    it { should have_db_column(:subscriber_count).of_type(:integer) }
+    it { should have_db_column(:channel_id).of_type(:string) }
+    it { should have_db_column(:title).of_type(:string) }
+    it { should have_db_column(:view_count).of_type(:integer) }
+    it { should have_db_column(:video_count).of_type(:integer) }
+    it { should have_db_column(:thumbnail).of_type(:string) }
+    it { should have_db_column(:published_at).of_type(:datetime) }
+    it { should have_db_column(:description).of_type(:string) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:channel_id) }
+    it { should validate_uniqueness_of(:channel_id) }
   end
 end
