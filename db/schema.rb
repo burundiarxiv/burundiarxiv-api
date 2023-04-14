5ec370294e70e4b49a1f86e2a50c7e04dbd0f40f
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2023-04-14 23:53:08 +0200

    Add description column to YouTubers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_214439) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_215236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_214439) do
     t.integer "video_count"
     t.string "thumbnail"
     t.datetime "published_at"
+    t.string "description"
   end
 
   add_foreign_key "datasets", "categories"
