9f2aa414c2721cb35f21449d9e5a020f8f1e3039
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2023-04-15 00:42:23 +0200

    Rename column 'thumbnail_url' to 'thumbnail' on videos table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_224043) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_224205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_224043) do
     t.bigint "youtuber_id", null: false
     t.string "title"
     t.string "description"
-    t.string "thumbnail_url"
+    t.string "thumbnail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "video_id"
