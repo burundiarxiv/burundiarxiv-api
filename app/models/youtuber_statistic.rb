83fe9eae51ad1010c5a4724dfb80647891163b19
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2023-04-17 23:42:19 +0200

    "Update YoutuberStatistic model to calculate views_difference dynamically"

--- app/models/youtuber_statistic.rb
+++ app/models/youtuber_statistic.rb
@@ -15,8 +15,7 @@ class YoutuberStatistic < ApplicationRecord
 
     return if previous_statistic.nil?
 
-    # views_difference = view_count - previous_statistic.view_count
-    views_difference = 4967
+    views_difference = view_count - previous_statistic.view_count
     min_earnings = ((views_difference * 0.25) / 1_000).round
     max_earnings = ((views_difference * 4.00) / 1_000).round
 
