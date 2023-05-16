3233f910762c8f8989a931d386d25dac7622b14a
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2023-05-16 17:52:28 +0200

    format

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -15,18 +15,20 @@ module Api
       private
 
       def game_params
-        params.require(:game).permit(
-          :country,
-          :end_time,
-          :score,
-          :solution,
-          :start_time,
-          :time_taken,
-          :timezone,
-          :won,
-          :latest,
-          guesses: [],
-        )
+        params
+          .require(:game)
+          .permit(
+            :country,
+            :end_time,
+            :score,
+            :solution,
+            :start_time,
+            :time_taken,
+            :timezone,
+            :won,
+            :latest,
+            guesses: [],
+          )
       end
     end
   end
