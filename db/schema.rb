8d7da26b4e284d99edf8bcf9effc54beaf1c2309
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2023-05-22 12:56:53 +0200

    Bump rails 7 (#72)

--- db/schema.rb
+++ db/schema.rb
@@ -10,107 +10,107 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_15_110730) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_22_105022) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'datasets', force: :cascade do |t|
-    t.bigint 'category_id', null: false
-    t.string 'name'
-    t.integer 'slug'
-    t.string 'path'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['category_id'], name: 'index_datasets_on_category_id'
+  create_table "datasets", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.string "name"
+    t.integer "slug"
+    t.string "path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_datasets_on_category_id"
   end
 
-  create_table 'games', force: :cascade do |t|
-    t.string 'solution'
-    t.text 'guesses', default: [], array: true
-    t.boolean 'won'
-    t.datetime 'start_time', precision: nil
-    t.datetime 'end_time', precision: nil
-    t.string 'country'
-    t.integer 'time_taken'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'timezone'
-    t.float 'score', default: 0.0
-    t.boolean 'latest', default: true
+  create_table "games", force: :cascade do |t|
+    t.string "solution"
+    t.text "guesses", default: [], array: true
+    t.boolean "won"
+    t.datetime "start_time", precision: nil
+    t.datetime "end_time", precision: nil
+    t.string "country"
+    t.integer "time_taken"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "timezone"
+    t.float "score", default: 0.0
+    t.boolean "latest", default: true
   end
 
-  create_table 'meanings', force: :cascade do |t|
-    t.string 'keyword', null: false
-    t.string 'meaning', null: false
-    t.string 'proverb', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "meanings", force: :cascade do |t|
+    t.string "keyword", null: false
+    t.string "meaning", null: false
+    t.string "proverb", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'missing_words', force: :cascade do |t|
-    t.string 'value'
-    t.integer 'count', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "missing_words", force: :cascade do |t|
+    t.string "value"
+    t.integer "count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'video_statistics', force: :cascade do |t|
-    t.bigint 'video_id', null: false
-    t.integer 'view_count'
-    t.integer 'like_count'
-    t.integer 'dislike_count'
-    t.integer 'favorite_count'
-    t.integer 'comment_count'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.date 'date', null: false
-    t.index ['video_id'], name: 'index_video_statistics_on_video_id'
+  create_table "video_statistics", force: :cascade do |t|
+    t.bigint "video_id", null: false
+    t.integer "view_count"
+    t.integer "like_count"
+    t.integer "dislike_count"
+    t.integer "favorite_count"
+    t.integer "comment_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date "date", null: false
+    t.index ["video_id"], name: "index_video_statistics_on_video_id"
   end
 
-  create_table 'videos', force: :cascade do |t|
-    t.bigint 'youtuber_id', null: false
-    t.string 'title'
-    t.string 'description'
-    t.string 'thumbnail'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'video_id'
-    t.datetime 'published_at'
-    t.index ['youtuber_id'], name: 'index_videos_on_youtuber_id'
+  create_table "videos", force: :cascade do |t|
+    t.bigint "youtuber_id", null: false
+    t.string "title"
+    t.string "description"
+    t.string "thumbnail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "video_id"
+    t.datetime "published_at"
+    t.index ["youtuber_id"], name: "index_videos_on_youtuber_id"
   end
 
-  create_table 'youtuber_statistics', force: :cascade do |t|
-    t.bigint 'youtuber_id', null: false
-    t.integer 'view_count'
-    t.integer 'video_count'
-    t.integer 'subscriber_count'
-    t.date 'date'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['youtuber_id'], name: 'index_youtuber_statistics_on_youtuber_id'
+  create_table "youtuber_statistics", force: :cascade do |t|
+    t.bigint "youtuber_id", null: false
+    t.integer "view_count"
+    t.integer "video_count"
+    t.integer "subscriber_count"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["youtuber_id"], name: "index_youtuber_statistics_on_youtuber_id"
   end
 
-  create_table 'youtubers', force: :cascade do |t|
-    t.integer 'subscriber_count'
-    t.string 'channel_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'title'
-    t.integer 'view_count'
-    t.integer 'video_count'
-    t.string 'thumbnail'
-    t.datetime 'published_at'
-    t.string 'description'
+  create_table "youtubers", force: :cascade do |t|
+    t.integer "subscriber_count"
+    t.string "channel_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.integer "view_count"
+    t.integer "video_count"
+    t.string "thumbnail"
+    t.datetime "published_at"
+    t.string "description"
   end
 
-  add_foreign_key 'datasets', 'categories'
-  add_foreign_key 'video_statistics', 'videos'
-  add_foreign_key 'videos', 'youtubers'
-  add_foreign_key 'youtuber_statistics', 'youtubers'
+  add_foreign_key "datasets", "categories"
+  add_foreign_key "video_statistics", "videos"
+  add_foreign_key "videos", "youtubers"
+  add_foreign_key "youtuber_statistics", "youtubers"
 end
