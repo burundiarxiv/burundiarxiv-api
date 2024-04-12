fb8981c0d3a6fb20d30831482f3295650d306645
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2024-04-12 19:05:31 +0200

    Add start_time to game_params and adjust model scopes

--- app/models/curura/game.rb
+++ app/models/curura/game.rb
@@ -1,7 +1,9 @@
-class Curura::Game < ApplicationRecord
-  validates :score, :country, :start_time, presence: true
+module Curura
+  class Game < ApplicationRecord
+    validates :score, :country, :start_time, presence: true
 
-  scope :today, -> { where("start_time >= ?", Time.zone.now.beginning_of_day) }
-  scope :won_above_score, ->(score) { where("score > ?", score) }
-  scope :country, ->(country) { where(country: country) }
+    scope :today, -> { where("start_time >= ?", Time.zone.now.beginning_of_day) }
+    scope :won_above_score, ->(score) { where("score > ?", score) }
+    scope :country, ->(country) { where(country: country) }
+  end
 end
