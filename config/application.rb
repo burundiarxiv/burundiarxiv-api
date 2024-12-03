e086699942df7eea5dbcb05072dc58cda606602e
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2024-12-03 11:55:07 +0100

    Update Rails default version to 7.0 and enable Zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module BurundiarxivApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
+    config.active_support.disable_to_s_conversion = false
+    config.autoloader = :zeitwerk
 
     # Configuration for the application, engines, and railties goes here.
     #
